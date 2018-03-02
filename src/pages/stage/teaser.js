13bd96436345d4aeee6ca665cc9d1671e79fa927
Lukas Bombach <lukas.bombach@googlemail.com>
2018-03-02 15:26:14 +0100

    styling

--- src/pages/stage/teaser.js
+++ src/pages/stage/teaser.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from 'react';
+import styles from './teaser.module.css';
 
 export default ({ article: { title, teaserImg, teaserText } }) =>
   <Fragment>
     <h2>{title}</h2>
-    { teaserImg ? <img src={teaserImg} alt="Teaser Bild" /> : null }
+    { teaserImg ? <img src={teaserImg} alt="Teaser Bild" className={styles.img} /> : null }
     { teaserText ? <p>{teaserText}</p> : null }
   </Fragment>
