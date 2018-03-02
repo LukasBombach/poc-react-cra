b412f11b372b0cf88fde5af36b0bec98eb04c0ee
Lukas Bombach <lukas.bombach@googlemail.com>
2018-03-02 16:20:12 +0100

    article styles

--- src/pages/article/head.js
+++ src/pages/article/head.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from 'react';
+import styles from './head.module.css';
 
 export default ({ article: { title, teaserImg, teaserText } }) =>
   <Fragment>
     <h1>{title}</h1>
-    { teaserImg ? <img src={teaserImg} alt="Teaser Bild" /> : null }
+    { teaserImg ? <img src={teaserImg} alt="Teaser Bild" className={styles.img} /> : null }
     { teaserText ? <p>{teaserText}</p> : null }
   </Fragment>
