25a8e58cf9f05b45ebfbbb147fd8d36a52fcd3a2
Lukas Bombach <lukas.bombach@googlemail.com>
2018-03-04 13:43:06 +0100

    teaser styles

--- src/pages/stage/teaser.js
+++ src/pages/stage/teaser.js
@@ -2,8 +2,6 @@ import React from 'react';
 import styles from './teaser.module.scss';
 
 export default ({ article: { id, title, teaserImg, teaserText } }) =>
-  <a href={`/article/${id}`}>
-    <h2 className={[styles.heading, 'theme-teaser--heading'].join(' ')}>{title}</h2>
-    { teaserImg ? <img src={teaserImg} alt="Teaser Bild" className={styles.img} /> : null }
-    { teaserText ? <p className={styles.teaser} s>{teaserText}</p> : null }
+  <a href={`/article/${id}`} className={styles.teaser} style={{ backgroundImage: `url(${teaserImg})` }}>
+    <h2 className="theme-teaser--heading">{title}</h2>
   </a>
