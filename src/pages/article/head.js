5ef42c42350c0aac55507bc18d27a8cd0d2a7b76
Lukas Bombach <lukas.bombach@googlemail.com>
2018-03-04 17:35:01 +0100

    amp article head

--- src/pages/article/head.js
+++ src/pages/article/head.js
@@ -1,9 +1,27 @@
 import React, { Fragment } from 'react';
+import Content from "./content";
 import styles from './head.module.scss';
 
-export default ({ article: { title, teaserImg, teaserText } }) =>
-  <Fragment>
-    <h1 className={[styles.heading, 'theme-teaser--heading'].join(' ')}>{title}</h1>
-    { teaserImg ? <img src={teaserImg} alt="Teaser Bild" className={styles.img} /> : null }
-    { teaserText ? <p className={styles.teaser}>{teaserText}</p> : null }
-  </Fragment>
+export default class Head extends Content {
+
+  renderWeb() {
+    return (
+      <Fragment>
+        <h1 className={[styles.heading, 'theme-teaser--heading'].join(' ')}>{this.props.article.title}</h1>
+        { this.props.article.teaserImg ? <img src={this.props.article.teaserImg} alt="Teaser Bild" className={styles.img} /> : null }
+        { this.props.article.teaserText ? <p className={styles.teaser}>{this.props.article.teaserText}</p> : null }
+      </Fragment>
+    );
+  }
+
+  renderAmp() {
+    return (
+      <Fragment>
+        <h1 className={[styles.heading, 'theme-teaser--heading'].join(' ')}>{this.props.article.title}</h1>
+        { this.props.article.teaserImg ? <amp-img src={this.props.article.teaserImg} layout="responsive" /> : null }
+        { this.props.article.teaserText ? <p className={styles.teaser}>{this.props.article.teaserText}</p> : null }
+      </Fragment>
+    );
+  }
+
+}
